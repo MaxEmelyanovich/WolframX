8cbc7694326dd253bf70cd40fb3145333c31c836
Drobyshevskyy <victordrob35@gmail.com>
2024-04-13 09:27:13 +0300

    front updated

--- front/my-app/src/components/SignInPage.js
+++ front/my-app/src/components/SignInPage.js
@@ -1,88 +1,92 @@
 import React from 'react';
 import './SignInPage.css';
-import wolframX from './assets/wolframx.svg';
+import wolframX from '../assets/wolframx.svg';
 
 class SignInPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          email: '',
-          password: ''
+            email: '',
+            password: '',
         };
-      }
-    
-      handleInputChange = (event) => {
+    }
+
+    handleInputChange = (event) => {
         this.setState({ [event.target.name]: event.target.value });
-      };
-    
-      handleSubmit = (event) => {
+    };
+
+    handleSubmit = (event) => {
         event.preventDefault();
-        
+
         const data = {
-          email: this.state.email,
-          password: this.state.password
+            email: this.state.email,
+            password: this.state.password,
         };
 
         fetch('http://25.23.19.72:8080/signin', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-          },
-          body: JSON.stringify(data)
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            },
+            body: JSON.stringify(data),
         })
-          .then(response => response.json())
-          .then(responseData => {
-            console.log('Ответ сервера:', responseData);
-            alert('ПОЛЬЗОВАТЕЛЬ АВТОРИЗОВАН');
-            // Дополнительные действия после успешной отправки данных
-          })
-          .catch(error => {
-            console.error('Ошибка при отправке данных:', error);
-          });
-        
+            .then((response) => response.json())
+            .then((responseData) => {
+                console.log('Ответ сервера:', responseData);
+                alert('ПОЛЬЗОВАТЕЛЬ АВТОРИЗОВАН');
+                // Дополнительные действия после успешной отправки данных
+            })
+            .catch((error) => {
+                console.error('Ошибка при отправке данных:', error);
+            });
+
         this.setState({ email: '', password: '' });
-      };
-    
-      render() {
+    };
+
+    render() {
         return (
-          <div className='authpage'>
-            <div className="wolframXdiv">
-              <img className="wolframX" src={wolframX} alt="WolframX" />
+            <div className="authpage">
+                <div className="wolframXdiv">
+                    <img className="wolframX" src={wolframX} alt="WolframX" />
+                </div>
+                <form type="formAuth" onSubmit={this.handleSubmit}>
+                    <label>
+                        <input
+                            type="signInPageInput"
+                            name="email"
+                            value={this.state.email}
+                            onChange={this.handleInputChange}
+                            placeholder="Email"
+                        />
+                    </label>
+                    <br />
+                    <label>
+                        <input
+                            type="signInPagePassword"
+                            name="password"
+                            value={this.state.password}
+                            onChange={this.handleInputChange}
+                            placeholder="Password"
+                        />
+                    </label>
+                    <br />
+                    <p type="signInPageP">
+                        <a type="signInPageRef1" href="/">
+                            Forgot password
+                        </a>
+                    </p>
+                    <button className="signInPageSubmit">Sign In</button>
+                    <p type="signInPageP2">
+                        Don't have an account?{' '}
+                        <a type="signInPageRef2" href="/signup">
+                            Sign Up
+                        </a>
+                    </p>
+                </form>
             </div>
-            <form type="formAuth" onSubmit={this.handleSubmit}>
-              <label>
-                <input
-                  type="signInPageInput"
-                  name="email"
-                  value={this.state.email}
-                  onChange={this.handleInputChange}
-                  placeholder="Email"
-                />
-              </label>
-              <br />
-              <label>
-                <input
-                  type="signInPagePassword"
-                  name="password"
-                  value={this.state.password}
-                  onChange={this.handleInputChange}
-                  placeholder="Password"
-                />
-              </label>
-              <br />
-              <p type="signInPageP">
-                <a type="signInPageRef1" href='/'>Forgot password</a> 
-              </p>
-              <button className="signInPageSubmit">Sign In</button>
-              <p type="signInPageP2">
-                Don't have an account?{' '}
-                <a type="signInPageRef2" href='/signup'>Sign Up</a> 
-              </p>
-            </form>
-          </div>
         );
-      }
+    }
 }
 
 // В signInPageRef1 поставить нужную ссылку на страницу для сброса пароля
--- front/my-app/src/components/SignUpPage.js
+++ front/my-app/src/components/SignUpPage.js
@@ -1,114 +1,115 @@
 import React from 'react';
 import './SignUpPage.css';
-import wolframX from './assets/wolframx.svg';
+import wolframX from '../assets/wolframx.svg';
 
 class SignUpPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             firstName: '',
-            lastName : '',
+            lastName: '',
             email: '',
-            password: ''
+            password: '',
         };
-      }
-    
-      handleInputChange = (event) => {
-        this.setState({ [event.target.name]: event.target.value });
-      };
-    
-      handleSubmit = (event) => {
+    }
 
+    handleInputChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    handleSubmit = (event) => {
         event.preventDefault();
 
         // Валидация полей
-  
+
         const data = {
-          firstName: this.state.firstName,
-          lastName: this.state.lastName,
-          email: this.state.email,
-          password: this.state.password
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            email: this.state.email,
+            password: this.state.password,
         };
 
         // http://25.23.19.72:8080/signup
         fetch('http://25.23.19.72:8080/signup', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-          },
-          body: JSON.stringify(data)
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            },
+            body: JSON.stringify(data),
         })
-          .then(response => response.json())
-          .then(responseData => {
-            console.log('Ответ сервера:', responseData);
-            alert('ПОЛЬЗОВАТЕЛЬ ЗАРЕГИСТРИРОВАН');
-            // Дополнительные действия после успешной отправки данных
-          })
-          .catch(error => {
-            console.error('Ошибка при отправке данных:', error);
-          });
+            .then((response) => response.json())
+            .then((responseData) => {
+                console.log('Ответ сервера:', responseData);
+                alert('ПОЛЬЗОВАТЕЛЬ ЗАРЕГИСТРИРОВАН');
+                // Дополнительные действия после успешной отправки данных
+            })
+            .catch((error) => {
+                console.error('Ошибка при отправке данных:', error);
+            });
 
         this.setState({
-          firstName: '',
-          lastName: '',
-          email: '',
-          password: ''
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
         });
-      };
-    
-      render() {
+    };
+
+    render() {
         return (
-          <div className='registration-page'>
-            <div className="wolframXdiv">
-              <img className="wolframX" src={wolframX} alt="WolframX" />
+            <div className="registration-page">
+                <div className="wolframXdiv">
+                    <img className="wolframX" src={wolframX} alt="WolframX" />
+                </div>
+                <form type="formReg" onSubmit={this.handleSubmit}>
+                    <label>
+                        <input
+                            type="signUpPageInput"
+                            name="firstName"
+                            value={this.state.firstName}
+                            onChange={this.handleInputChange}
+                            placeholder="First Name"
+                        />
+                    </label>
+                    <label>
+                        <input
+                            type="signUpPageInput"
+                            name="lastName"
+                            value={this.state.lastName}
+                            onChange={this.handleInputChange}
+                            placeholder="Last Name"
+                        />
+                    </label>
+                    <label>
+                        <input
+                            type="signUpPageInput"
+                            name="email"
+                            value={this.state.email}
+                            onChange={this.handleInputChange}
+                            placeholder="Email"
+                        />
+                    </label>
+                    <label>
+                        <input
+                            type="signUpPagePassword"
+                            name="password"
+                            value={this.state.password}
+                            onChange={this.handleInputChange}
+                            placeholder="Password"
+                        />
+                    </label>
+                    <button className="signUpPageSubmit">Sign Up</button>
+                    <p type="signUpPageP">
+                        Already have an account?{' '}
+                        <a type="signUpPageRef" href="/signin">
+                            Sign In
+                        </a>
+                    </p>
+                </form>
             </div>
-            <form type="formReg" onSubmit={this.handleSubmit}>
-              <label>
-                <input
-                  type="signUpPageInput"
-                  name="firstName"
-                  value={this.state.firstName}
-                  onChange={this.handleInputChange}
-                  placeholder="First Name"
-                />
-              </label>
-              <label>
-                <input
-                  type="signUpPageInput"
-                  name="lastName"
-                  value={this.state.lastName}
-                  onChange={this.handleInputChange}
-                  placeholder="Last Name"
-                />
-              </label>
-              <label>
-                <input
-                  type="signUpPageInput"
-                  name="email"
-                  value={this.state.email}
-                  onChange={this.handleInputChange}
-                  placeholder="Email"
-                />
-              </label>
-              <label>
-                <input
-                  type="signUpPagePassword"
-                  name="password"
-                  value={this.state.password}
-                  onChange={this.handleInputChange}
-                  placeholder="Password"
-                />
-              </label>
-              <button className="signUpPageSubmit">Sign Up</button>
-              <p type="signUpPageP">
-                Already have an account?{' '}
-                <a type="signUpPageRef" href='/signin'>Sign In</a>
-              </p>
-            </form>
-          </div>
         );
-      }
+    }
 }
 
 export default SignUpPage;
