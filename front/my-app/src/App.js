7e6fea1e4d3bb488dac37940f689ab7ae08a68ee
Drobyshevskyy <victordrob35@gmail.com>
2024-05-21 22:44:56 +0300

    front updated

--- front/my-app/src/App.js
+++ front/my-app/src/App.js
@@ -1,10 +1,123 @@
-import React from 'react';
+// import React from 'react';
+// import {
+//     BrowserRouter as Router,
+//     Link,
+//     Routes,
+//     Route,
+//     useLocation,
+// } from 'react-router-dom';
+// import { useTranslation } from 'react-i18next';
+// import LanguageSelector from './components/LanguageSelector';
+// import SignInPage from './components/SignInPage';
+// import SignUpPage from './components/SignUpPage';
+// import Calculations from './components/Calculations';
+// import MatrixTranspose from './components/MatrixTranspose';
+// import MatrixMultiplication from './components/MatrixMultiplication';
+// import MatrixAddition from './components/MatrixAddition';
+// import MatrixSubtraction from './components/MatrixSubtraction';
+// import ScalarMultiplication from './components/ScalarMultiplication';
+// import ChatRoom from './components/ChatRoom';
+// import Calculator from './components/Calculator';
+// import Trapezoidal from './components/Trapezoidal';
+// import Simpson from './components/Simpson';
+// import Romberg from './components/Romberg';
+// import Converter from './components/Converter';
+// import VectorPage from './components/VectorPage';
+// import VectorSecondPage from './components/VectorSecondPage';
+// import VectorModule from './components/VectorModule';
+// import Gauss from './components/Gauss';
+// import Newton from './components/Newton';
+// import './App.css';
+// import logoSVG from './assets/logo.svg';
+
+// function App() {
+//     return (
+//         <Router>
+//             <AppContent />
+//         </Router>
+//     );
+// }
+
+// function AppContent() {
+//     const location = useLocation();
+//     const isMainPage = location.pathname === '/';
+
+//     const { t } = useTranslation();
+
+//     return (
+//         <div>
+//             {isMainPage && (
+//                 <nav className="main-nav">
+//                     <img className="logoSVG" src={logoSVG} alt="Logo" />
+//                     <Link to="/" className="about-button">
+//                         {t('about')}
+//                     </Link>
+//                     <Link to="/calculations" className="calculations-button">
+//                         {t('calculations')}
+//                     </Link>
+//                     <Link to="/signup" className="signup-button">
+//                         {t('signup')}
+//                     </Link>
+//                     <div className="startPageSelector">
+//                     <LanguageSelector />
+//                     </div>
+//                 </nav>
+//             )}
+
+//             {/* <h1 className="mainLabel">Your personal calculating assistant</h1> */}
+
+//             {isMainPage && (
+//                 <div className="secondary-buttons">
+//                     <Link to="/signin" className="signin-button">
+//                         {t('signin')}
+//                     </Link>
+//                 </div>
+//             )}
+
+//             <Routes>
+//                 <Route path="/signup" element={<SignUpPage />} />
+//                 <Route path="/signin" element={<SignInPage />} />
+//                 <Route path="/calculations" element={<Calculations />} />
+//                 <Route path="/matrixtranspose" element={<MatrixTranspose />} />
+//                 <Route
+//                     path="/matrixmultiplication"
+//                     element={<MatrixMultiplication />}
+//                 />
+//                 <Route path="/matrixaddition" element={<MatrixAddition />} />
+//                 <Route
+//                     path="/matrixsubtraction"
+//                     element={<MatrixSubtraction />}
+//                 />
+//                 <Route
+//                     path="/scalarmultiplication"
+//                     element={<ScalarMultiplication />}
+//                 />
+//                 <Route path="/chatroom" element={<ChatRoom />}/>
+//                 <Route path="/calculator" element={<Calculator />}/>
+//                 <Route path="/trapezoidal" element={<Trapezoidal />}/>
+//                 <Route path="/simpson" element={<Simpson />}/>
+//                 <Route path="/romberg" element={<Romberg />}/>
+//                 <Route path="/converter" element={<Converter />}/>
+//                 <Route path="/vectorpage" element={<VectorPage />}/>
+//                 <Route path="/vectorsecondpage" element={<VectorSecondPage />}/>
+//                 <Route path="/vectormodule" element={<VectorModule />}/>
+//                 <Route path="/gauss" element={<Gauss />}/>
+//                 <Route path="/newton" element={<Newton />}/>
+//             </Routes>
+//         </div>
+//     );
+// }
+
+// export default App;
+
+import React, { useState, useEffect } from 'react';
 import {
     BrowserRouter as Router,
     Link,
     Routes,
     Route,
     useLocation,
+    useNavigate,
 } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import LanguageSelector from './components/LanguageSelector';
@@ -31,52 +144,85 @@ import './App.css';
 import logoSVG from './assets/logo.svg';
 
 function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const username = localStorage.getItem('username');
+        if (username) {
+            setIsLoggedIn(true);
+        }
+    }, []);
+
     return (
         <Router>
-            <AppContent />
+            <AppContent isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} /> 
         </Router>
     );
 }
 
-function AppContent() {
+function AppContent({ isLoggedIn, setIsLoggedIn }) {
+    const navigate = useNavigate();
     const location = useLocation();
     const isMainPage = location.pathname === '/';
 
     const { t } = useTranslation();
 
+    const handleSignOut = () => {
+        localStorage.removeItem('username');
+        setIsLoggedIn(false); 
+        navigate('/');
+    };
+
     return (
         <div>
             {isMainPage && (
                 <nav className="main-nav">
+                    
                     <img className="logoSVG" src={logoSVG} alt="Logo" />
+                    
+                    
                     <Link to="/" className="about-button">
                         {t('about')}
                     </Link>
                     <Link to="/calculations" className="calculations-button">
                         {t('calculations')}
                     </Link>
-                    <Link to="/signup" className="signup-button">
-                        {t('signup')}
-                    </Link>
+                    {isLoggedIn ? (
+                        <button className="signout-button" onClick={handleSignOut}>
+                            {t('signout')}
+                        </button>
+                    ) : (
+                        <>
+                        <div style={{display: 'flex', flexDirection: 'column'}}>
+                        <Link to="/signup" className="signup-button">
+                                {t('signup')}
+                            </Link>
+                            
+                        </div>
+                            
+                            
+                        </>
+                    )}
+                    
                     <div className="startPageSelector">
-                    <LanguageSelector />
+                        <LanguageSelector />
                     </div>
+                    
+                    
                 </nav>
             )}
 
             {/* <h1 className="mainLabel">Your personal calculating assistant</h1> */}
 
-            {isMainPage && (
-                <div className="secondary-buttons">
-                    <Link to="/signin" className="signin-button">
-                        {t('signin')}
-                    </Link>
-                </div>
+            {isMainPage && !isLoggedIn && (
+                <Link to="/signin" className="signin-button">
+                {t('signin')}
+            </Link>
             )}
 
             <Routes>
                 <Route path="/signup" element={<SignUpPage />} />
-                <Route path="/signin" element={<SignInPage />} />
+                <Route path="/signin" element={<SignInPage setIsLoggedIn={setIsLoggedIn} />} />
                 <Route path="/calculations" element={<Calculations />} />
                 <Route path="/matrixtranspose" element={<MatrixTranspose />} />
                 <Route
@@ -92,17 +238,20 @@ function AppContent() {
                     path="/scalarmultiplication"
                     element={<ScalarMultiplication />}
                 />
-                <Route path="/chatroom" element={<ChatRoom />}/>
-                <Route path="/calculator" element={<Calculator />}/>
-                <Route path="/trapezoidal" element={<Trapezoidal />}/>
-                <Route path="/simpson" element={<Simpson />}/>
-                <Route path="/romberg" element={<Romberg />}/>
-                <Route path="/converter" element={<Converter />}/>
-                <Route path="/vectorpage" element={<VectorPage />}/>
-                <Route path="/vectorsecondpage" element={<VectorSecondPage />}/>
-                <Route path="/vectormodule" element={<VectorModule />}/>
-                <Route path="/gauss" element={<Gauss />}/>
-                <Route path="/newton" element={<Newton />}/>
+                <Route path="/chatroom" element={<ChatRoom />} />
+                <Route path="/calculator" element={<Calculator />} />
+                <Route path="/trapezoidal" element={<Trapezoidal />} />
+                <Route path="/simpson" element={<Simpson />} />
+                <Route path="/romberg" element={<Romberg />} />
+                <Route path="/converter" element={<Converter />} />
+                <Route path="/vectorpage" element={<VectorPage />} />
+                <Route
+                    path="/vectorsecondpage"
+                    element={<VectorSecondPage />}
+                />
+                <Route path="/vectormodule" element={<VectorModule />} />
+                <Route path="/gauss" element={<Gauss />} />
+                <Route path="/newton" element={<Newton />} />
             </Routes>
         </div>
     );
--- front/my-app/src/components/SignInPage.js
+++ front/my-app/src/components/SignInPage.js
@@ -1,107 +1,15 @@
-// import React from 'react';
-// import './SignInPage.css';
-// import wolframX from '../assets/wolframx.svg';
-// import { useTranslation } from 'react-i18next';
-
-// class SignInPage extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             email: '',
-//             password: '',
-//         };
-//     }
-
-//     handleInputChange = (event) => {
-//         this.setState({ [event.target.name]: event.target.value });
-//     };
-
-//     handleSubmit = (event) => {
-//         event.preventDefault();
-
-//         const data = {
-//             email: this.state.email,
-//             password: this.state.password,
-//         };
-
-//         fetch('http://25.23.19.72:8080/signin', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 Accept: 'application/json',
-//             },
-//             body: JSON.stringify(data),
-//         })
-//             .then((response) => response.json())
-//             .then((responseData) => {
-//                 console.log('Ответ сервера:', responseData);
-//                 alert('ПОЛЬЗОВАТЕЛЬ АВТОРИЗОВАН');
-//                 // Дополнительные действия после успешной отправки данных
-//             })
-//             .catch((error) => {
-//                 console.error('Ошибка при отправке данных:', error);
-//             });
-
-//         this.setState({ email: '', password: '' });
-//     };
-
-//     render() {
-//         return (
-//             <div className="authpage">
-//                 <div className="wolframXdiv">
-//                     <img className="wolframX" src={wolframX} alt="WolframX" />
-//                 </div>
-//                 <form type="formAuth" onSubmit={this.handleSubmit}>
-//                     <label>
-//                         <input
-//                             type="signInPageInput"
-//                             name="email"
-//                             value={this.state.email}
-//                             onChange={this.handleInputChange}
-//                             placeholder="Email"
-//                         />
-//                     </label>
-//                     <br />
-//                     <label>
-//                         <input
-//                             type="signInPagePassword"
-//                             name="password"
-//                             value={this.state.password}
-//                             onChange={this.handleInputChange}
-//                             placeholder="Password"
-//                         />
-//                     </label>
-//                     <br />
-//                     <p type="signInPageP">
-//                         <a type="signInPageRef1" href="/">
-//                             Forgot password
-//                         </a>
-//                     </p>
-//                     <button className="signInPageSubmit">Sign In</button>
-//                     <p type="signInPageP2">
-//                         Don't have an account?{' '}
-//                         <a type="signInPageRef2" href="/signup">
-//                             Sign Up
-//                         </a>
-//                     </p>
-//                 </form>
-//             </div>
-//         );
-//     }
-// }
-
-// // В signInPageRef1 поставить нужную ссылку на страницу для сброса пароля
-
-// export default SignInPage;
-
 import React, { useState } from 'react';
 import './SignInPage.css';
 import wolframX from '../assets/wolframx.svg';
 import { useTranslation } from 'react-i18next';
 import LanguageSelector from './LanguageSelector';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-const SignInPage = () => {
+const SignInPage = ({ setIsLoggedIn }) => {
+
+    const navigate = useNavigate();
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const handleInputChange = (event) => {
@@ -131,10 +39,15 @@ const SignInPage = () => {
             .then((response) => response.json())
             .then((responseData) => {
                 console.log('Ответ сервера:', responseData);
-                alert('ПОЛЬЗОВАТЕЛЬ АВТОРИЗОВАН');
+                alert('HELLO, ' + responseData.firstName + ' ' + responseData.lastName);
+                
                 // Дополнительные действия после успешной отправки данных
                 const username = responseData.firstName;
                 localStorage.setItem('username', username);
+
+                navigate('/');
+                setIsLoggedIn(true);
+                
             })
             .catch((error) => {
                 console.error('Ошибка при отправке данных:', error);
@@ -197,3 +110,4 @@ const SignInPage = () => {
 };
 
 export default SignInPage;
+
