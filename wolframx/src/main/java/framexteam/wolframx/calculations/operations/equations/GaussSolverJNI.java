8c09de53a6e9e97cb29fb750eb682ce65cb4522a
VadimKasyanenko <113146148+VadimKasyanenko@users.noreply.github.com>
2024-05-22 13:46:54 +0300

    Add files via upload

--- wolframx/src/main/java/framexteam/wolframx/calculations/operations/equations/GaussSolverJNI.java
+++ wolframx/src/main/java/framexteam/wolframx/calculations/operations/equations/GaussSolverJNI.java
@@ -1,9 +1,11 @@
 package framexteam.wolframx.calculations.operations.equations;
 
 public class GaussSolverJNI {
+
     static {
         System.loadLibrary("GaussSolver"); // Загрузка библиотеки JNI
     }
     public native static double[] solve(double[][] coefficients, double[] constants, int threadCount);
+    public native static long getExecutionTime();
 
 }
